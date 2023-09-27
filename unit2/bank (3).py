6f8231aba92ae98f74a70e559ef935171ef40175
Vinothkumar06032003 <144656932+Vinothkumar06032003@users.noreply.github.com>
2023-09-27 00:21:04 -0700

    Add files via upload

---
+++ unit2/bank (3).py
@@ -0,0 +1,32 @@
+class BankAccount:
+
+	def __init__(self, account_number,account_holder_name, initial_balance=0.0):
+		self.__account_number = account_number
+		self.__account_holder_name = account_holder_name
+		self.__account_balance = initial_balance
+
+	def deposit(self, amount):
+		if amount > 0:
+			self.__account_balance += amount
+			print("Deposited ₹{}. New balance : ₹{}".format(amount, self.__account_balance))
+		else:
+			print("Invalid deposit amount. Please deposit a correct amount.")
+
+	def withdraw(self, amount):
+		if amount > 0 and amount <= self.__account_balance:
+			self.__account_balance -= amount
+			print("Withdraw ₹{}. New balance: ₹{}".format(amount, self.__account_balance))
+		else:
+			print("Invalid withdrawal amount or insufficient balance.")
+
+	def display_balance(self):
+		print("Account balance for {} (Account #{}): ₹{}".format(self.__account_holder_name,self.__account_number,self.__account_balance))
+
+
+account = BankAccount(account_number = "0987654321",account_holder_name = "Thala Ajith",initial_balance=500000.0)
+
+account.display_balance()
+account.deposit(100000.0)
+account.withdraw(70000.0)
+account.display_balance()
+
